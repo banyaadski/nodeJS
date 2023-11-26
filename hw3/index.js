e9201f8043d306fbe1d89dbf3e386634adaac9eb
banyaadski <89918249+banyaadski@users.noreply.github.com>
2023-11-26 13:29:01 +0300

    hw3

---
+++ hw3/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+// const path = require("path");
+const fs = require("fs");
+
+const app = express();
+// app.use(express.static("./static"));
+
+app.get("/", (req, res) => {
+  const data = fs.readFileSync("./json/counter.json", "utf-8");
+  let dataObj = JSON.parse(data);
+  dataObj.main = dataObj.main + 1;
+  fs.writeFileSync("./json/counter.json", JSON.stringify(dataObj, null, 3));
+  res.send(
+    `<h1>Main </h1>\ <a href ="/about">To about</a> \<h1>Число открытий страницы ${dataObj.main}</h1>`
+  );
+});
+app.get("/about", (req, res) => {
+  const data = fs.readFileSync("./json/counter.json", "utf-8");
+  let dataObj = JSON.parse(data);
+  dataObj.counter = dataObj.counter + 1;
+  fs.writeFileSync("./json/counter.json", JSON.stringify(dataObj, null, 3));
+  res.send(
+    `<h1>About </h1> <a href ="/">To main</a> \<h1>Число открытий страницы ${dataObj.counter}</h1>`
+  );
+});
+
+
+
+app.listen(3000);
